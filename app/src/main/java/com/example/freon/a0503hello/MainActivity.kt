033c2b4596fdfc819e15e2df7aad618e30136831
Mikhail Lapin <mlapin@rambler.ru>
2020-11-23 14:10:03 +0300

    a0503hello

---
+++ app/src/main/java/com/example/freon/a0503hello/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.freon.a0503hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
